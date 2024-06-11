0838c5f39dcf0010cef88c1c974cdae39e82074d
ekinhalisdemir <161486682+ekinhalisdemir@users.noreply.github.com>
2024-06-11 17:47:16 +0300

    Add files via upload

---
+++ src/pl/pp/MyFirstApp.java
@@ -0,0 +1,30 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public
+class MyFirstApp {
+    public static void main(String[] args) {
+        double x = 10;
+        double y = 2;
+        Scanner scanner = new Scanner(System.in);
+        var result = x + y;
+        System.out.println("x + y = " + result);
+        result = x - y;
+        System.out.println("x - y = " + result);
+        result = x * y;
+        System.out.println("x * y = " + result);
+        result = x / y;
+        System.out.println("x / y = " + result);
+        result = x % y;
+        System.out.println("x % y = " + result);
+        System.out.println("Enter two numbers separated by the Enter key:");
+        double first = scanner.nextDouble();
+        double second = scanner.nextDouble();
+        System.out.println("x + y = " + (first + second));
+        System.out.println("x - y = " + (first - second));
+        System.out.println("x * y = " + (first * second));
+        System.out.println("x / y = " + (first / second));
+
+    }
+}
