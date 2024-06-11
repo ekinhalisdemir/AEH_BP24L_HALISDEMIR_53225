a0d1f44b00c1bf77011329509ac91dad3bd2b854
ekinhalisdemir <161486682+ekinhalisdemir@users.noreply.github.com>
2024-06-11 18:32:47 +0300

    Add files via upload

---
+++ src/pl/pp/Main.java
@@ -0,0 +1,12 @@
+public static void main() {
+    printCharacter('X', 9, 7);
+}
+
+public static void printCharacter(char character, int numberInRow, int numberOfLines) {
+    for (int i = 0; i < numberOfLines; i++) {
+        for (int j = 0; j < numberInRow; j++) {
+            System.out.print(character);
+        }
+        System.out.println();
+    }
+}
